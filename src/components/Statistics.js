79ad7ed757b5e0dfcca55eff9f2948c8230d8401
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-04-25 00:21:00 +0300

    Statistucs styles

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
-
+import css from '././Statistics/Statistics.module.css';
 const Statistics = ({stats, title}) => {
     
         return (
-    <section className="statistics">
-      <h2 className="title">{title}</h2>
-        <ul className="stat-list">
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
+        <ul className={css.stat_list}>
          {stats.map(stat => (
-        <li className="item" key={stat.id}>
-          <span className="label">{stat.label}</span>
-          <span className="percentage">{stat.percentage}%</span>
+        <li className={css.item} key={stat.id}>
+          <span className={css.label}>{stat.label}</span>
+          <span className={css.percentage}>{stat.percentage}%</span>
         </li>))}
         </ul>
     </section>
