dbd29444eb43ed48a78ea8394f62d98a0d017843
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-04-27 20:30:59 +0300

    fin

--- src/components/utils/makeColorsStatistics.js
+++ src/components/utils/makeColorsStatistics.js
@@ -1,4 +1,15 @@
 export default function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    const color =
+    'rgba(' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    0.5 +
+    ')';
+
+  return color;
   }
   
