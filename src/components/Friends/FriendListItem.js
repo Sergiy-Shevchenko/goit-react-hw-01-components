f80985f69474942a70bc71e901fa70a4296ebab0
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-05-01 20:16:26 +0300

    refactoring

--- src/components/Friends/FriendListItem.js
+++ src/components/Friends/FriendListItem.js
@@ -6,19 +6,15 @@ const FriendListItem = ({friend}) => {
         const {isOnline, avatar, name} = friend;
          return (
             <li className={css.item}>
-            <span className={isOnline ? css.statusOnline : css.statusOffline}></span>
+        <span className={isOnline ? css.statusOnline : css.statusOffline}></span>
             <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
              <p className={css.name}>{name}</p>
 </li>)}
        
 FriendListItem.propTypes = {
-   friend: PropTypes.arrayOf(
-      PropTypes.exact({
-        avatar: PropTypes.string,
-        name: PropTypes.string,
-        isOnline: PropTypes.bool,
-      }).isRequired,
-    ).isRequired,
-  };
+        avatar: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        isOnline: PropTypes.bool.isRequired,
+     };
 
 export default FriendListItem;
